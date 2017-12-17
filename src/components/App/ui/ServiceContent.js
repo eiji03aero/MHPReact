7f0fc43d7bfba69cfd9aecb6829b2ab4fbddfba7
eiji03aero <eiji03aero@gmail.com>
2017-12-17 22:34:49 +0900

    third

--- src/components/App/ui/ServiceContent.js
+++ src/components/App/ui/ServiceContent.js
@@ -3,7 +3,7 @@ import { AnimatedSwitch } from 'react-router-transition'
 
 import WelcomePage from '../../WelcomePage/WelcomePage.js'
 import CommentLists from '../../CommentLists/CommentLists.js'
-import ImageMap from '../../ImageMap/ImageMap.js'
+import ImageMap from '../../ImageMapdesu/ImageMap.js'
 import Wiki from '../../Wikidesu/Wiki.js'
 import ColorOrganizer from '../../ColorOrganizerdesu/ColorOrganizer.js'
 import Whoops404 from '../../common/error/Whoops404.js'
