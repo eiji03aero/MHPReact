156e9b72dac6a826e807a8f84fc11b78e7ea9ffa
eiji03aero <eiji03aero@gmail.com>
2017-12-17 18:05:54 +0900

    connects #57 finished refactoring header

--- src/components/App/ui/ServiceHeader.js
+++ src/components/App/ui/ServiceHeader.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
+import LinkIcon from '../../common/util/LinkIcon/LinkIcon.js'
+
 import '../stylesheets/ServiceHeader.scss'
 
 const ServiceHeader = () =>
-  <div className="serviceHeader">
-    <h3>here comes a header</h3>
+  <div className="ServiceHeader u-flex--fs">
+    <span className="u-fs--24">MHPReact</span>
+    <div className="u-optional-right">
+      <LinkIcon
+        title="Home"
+        icon="home"
+        to="/" />
+    </div>
   </div>
 
 export default ServiceHeader
