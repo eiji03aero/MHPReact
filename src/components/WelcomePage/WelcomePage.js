f286a0ae378eb94fc77430582cffef06be4ecdb7
eiji03aero <eiji03aero@gmail.com>
2017-12-22 17:05:44 +0400

    finished revise design

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,27 +1,9 @@
-import { connect } from 'react-redux'
-import { getTest } from '../../redux/actions/welcomePage.js'
 import './Welcome.scss'
 
-const mapStateToProps = state =>
-  ({
-    message: state.welcomePage.message
-  })
-
-const mapDispatchToProps = dispatch =>
-  ({
-    onClick () {
-      dispatch(getTest())
-    }
-  })
-
-const WelcomePage = ({ message, onClick }) =>
+const WelcomePage = () =>
   <div className="welcome-container">
     <h1>welcome to my page!</h1>
     <p>make yourself comfortable around here!</p>
-    <p>
-      <button onClick={ onClick }>click me!</button>
-    </p>
-    <p>{ message }</p>
   </div>
 
-export default connect(mapStateToProps, mapDispatchToProps)(WelcomePage)
+export default WelcomePage
--- src/redux/reducers/welcomePage.js
+++ src/redux/reducers/welcomePage.js
@@ -2,12 +2,6 @@ import C from '../constants.js'
 
 const welcomePage = (state = {}, action) => {
   switch (action.type) {
-    case (C.GET_TEST) :
-      console.log('gets called')
-      return {
-        ...state,
-        message: action.message
-      }
 
     default :
       return state
