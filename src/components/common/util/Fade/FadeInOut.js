a296a91409904fa8580fe810a3a5e13421942a6e
eiji03aero <eiji03aero@gmail.com>
2017-12-26 12:37:04 -0300

    little more

---
+++ src/components/common/util/Fade/FadeInOut.js
@@ -0,0 +1,33 @@
+import { Transition } from 'react-transition-group'
+
+const duration = 100
+
+const defaultStyles = {
+  display: 'inline-block',
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 1 },
+  exited: { opacity: 0 }
+}
+
+const FadeInOut =({ in: inProp, children }) => {
+  console.log(inProp)
+  return (
+    <Transition in={ inProp } timeout={ duration }>
+      {(state) =>(
+        <div style={{
+          ...defaultStyles,
+          ...transitionStyles[state] }}>
+          { children }
+        </div>
+      )}
+    </Transition>
+  )
+}
+
+export default FadeInOut
